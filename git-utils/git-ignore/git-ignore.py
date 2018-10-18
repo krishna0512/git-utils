2544051f837ddbcd9871880b14fe697730155bf9
Krishna Tulsyan <kt.krishna.tulsyan@gmail.com>
2018-10-18 19:02:28 +0530

    added the gitignore github submodule in data folder for git-ignore

--- git-utils/git-ignore/git-ignore.py
+++ git-utils/git-ignore/git-ignore.py
@@ -5,13 +5,25 @@ import sys
 # for touch functionality
 from pathlib import Path
 
+def display_help():
+    """
+    Displays the help message
+    """
+    pass
+
 def main():
     links = {
             'python':'https://raw.githubusercontent.com/github/gitignore/master/Python.gitignore'
             }
-    if sys.argv[1] not in links:
-        print('Command not recognized. {}'.format(sys.argv[1]))
-    data = requests.get(links[sys.argv[1]]).text
+    options = [i.lower() for i in sys.argv]
+    if len(options) == 1 or options[1] == 'help':
+        display_help()
+        return
+    if options[1] not in links:
+        print('Command not recognized. {}'.format(options[1]))
+        return
+
+    data = requests.get(links[options[1]]).text
     p = os.getcwd() + '/.gitignore'
     Path(p).touch()
     with open(p,'w') as f:
