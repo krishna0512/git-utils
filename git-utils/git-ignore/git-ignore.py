71e312a2c7db8b85d7fc7e6aafcafda3d124bc73
Krishna Tulsyan <kt.krishna.tulsyan@gmail.com>
2018-10-18 20:08:30 +0530

    added the install for installing the git-ignore to the .local/bin and updated git-ignore to work for all

--- git-utils/git-ignore/git-ignore.py
+++ git-utils/git-ignore/git-ignore.py
@@ -11,20 +11,30 @@ def display_help():
     """
     pass
 
+def load_links():
+    p = Path(__file__).resolve().parent
+    p = p / 'data'
+    p = p.resolve()
+    a = os.listdir(str(p))
+    return {i.lower().strip().split('.')[0]:(p / i).resolve() for i in a if 'gitignore' in i}
+
 def main():
-    links = {
-            'python':'https://raw.githubusercontent.com/github/gitignore/master/Python.gitignore'
-            }
     options = [i.lower() for i in sys.argv]
     if len(options) == 1 or options[1] == 'help':
         display_help()
         return
+    links = load_links()
     if options[1] not in links:
         print('Command not recognized. {}'.format(options[1]))
         return
 
-    data = requests.get(links[options[1]]).text
+    with links[options[1]].open() as f:
+        r = f.readlines()
+    r = [i.strip() for i in r]
+    data = '\n'.join(r) + '\n'
     p = os.getcwd() + '/.gitignore'
+    if os.path.exists(p):
+        os.remove(p)
     Path(p).touch()
     with open(p,'w') as f:
         f.write(data)
